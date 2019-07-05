05cd52b4cfc1e23a1266cfac7b752479b188afb8
Felix Delattre <felix@delattre.de>
2019-07-05 15:25:57 +0000

    Implement services api

--- service/src/main/java/de/schildbach/pte/service/Application.java
+++ service/src/main/java/de/schildbach/pte/service/Application.java
@@ -19,11 +19,56 @@ package de.schildbach.pte.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+// import java.lang.Class;
+// import java.lang.reflect.Constructor;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import de.schildbach.pte.AbstractNetworkProvider;
+import de.schildbach.pte.NicaraguaProvider;
+
+/**
+ * @author Felix Delattre
+ */
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
+        /* Start the main web service application */
         SpringApplication.run(Application.class, args);
     }
+
+    public static AbstractNetworkProvider get_provider() {
+
+        /* Read configuration about provider and additional information related from config.json */
+        String provider = "", token = "";
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader("config.json")) {
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            provider = (String) jsonObject.get("provider");
+            token = (String) jsonObject.get("token");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // TODO: Create dynamically the provider object from configuration
+        // 
+        // Class<?> clazz = Class.forName("de.schildbach.pte." + provider + "Provider");
+        // Class<?>[] parameters = new Class[] {String.class};
+        // Constructor<?> constructor = clazz.getConstructor(parameters);
+        // Object instance = constructor.newInstance(new Object[] {authorization});
+        // if(instance instanceof AbstractNetworkProvider) {
+        //    return instance;
+        //}
+        return new NicaraguaProvider(token);
+    }
 }
+
