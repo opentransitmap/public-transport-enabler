4616c262b97791e4a269c3c18175b8abdcd87585
Felix Delattre <felix@delattre.de>
2019-07-08 21:01:48 +0000

    Select provider for service from configuration file

--- service/src/main/java/de/schildbach/pte/service/Application.java
+++ service/src/main/java/de/schildbach/pte/service/Application.java
@@ -17,8 +17,21 @@
 
 package de.schildbach.pte.service;
 
+import de.schildbach.pte.AbstractNetworkProvider;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 
 /**
  * @author Felix Delattre
@@ -28,5 +41,49 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    public static AbstractNetworkProvider get_provider() {
+
+        /* Read configuration about provider and additional information related from config.json */
+        String provider = "", token = "";
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader("service/config.json")) {
+            JSONObject jsonObject = (JSONObject) parser.parse(reader);
+            provider = (String) jsonObject.get("provider");
+            token = (String) jsonObject.get("token");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        /* Create Provider object from dynamic class name obtained from configuration above */
+        Object object = null;
+        try {
+            Class<?> classDefinition = Class.forName("de.schildbach.pte." + provider + "Provider");
+
+            if (token != "") {
+                Constructor<?> classConstructor = classDefinition.getConstructor(String.class);
+                object = classConstructor.newInstance(new Object[] { token });
+            }
+            else {
+                object = classDefinition.newInstance();
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    
+        return (AbstractNetworkProvider) object;
     }
 }
